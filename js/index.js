097a91ffe87ab36a5af50522c9c46dadf57989d3
vittoriaromano <vittoria5005@gmail.com>
2023-03-02 19:31:16 +0400

    griglia-completa

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
-alert('wewe')
+'use strict'
+
+//functions
+
+
+//funzione per creare cella
+function myCreateElement(htmlElement,className,htmlValue){
+    const element = document.createElement(htmlElement);
+    element.classList.add(className);
+    element.innerText = htmlValue;
+     
+
+    //al click cambiamo una classe colore e console.log la proprieta html
+    element.addEventListener('click', function(){
+        console.log(htmlValue);
+        element.classList.add('colore');
+
+    })
+    return element;
+}
+//appendere cella al html
+function myAppendElement(containerElement, htmlElement){
+    containerElement.append(htmlElement);
+}
+
+//main
+
+//ceare le celle x 100
+let cellNumber = '100'
+const containerBoard = document.querySelector('.board');
+
+//far mostrare le celle al click
+ const btn = document.querySelector('.btn');
+
+ btn.addEventListener('click', function(){
+    
+     for(let i = 1; i <= cellNumber; i++){
+        const createdElement = myCreateElement('div', 'cell', i);
+        myAppendElement(containerBoard, createdElement);
+      
+    }
+
+  } )
+
+
--- readme.md
+++ readme.md
@@ -1,18 +1,18 @@
 1) Consegna:
 
 - L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-//creare un bottone 
-//creare la grida e nasconderla
+//creare un bottone /
+//creare la grida e nasconderla/
 //al click del bottone si mostrera' la gligia 
   //la griglia sara' un elemento integrato nel css grazie alla funzione js di js
 
 
 - Ogni cella ha un numero progressivo, da 1 a 100.
-// aggiungiamo un index <= a 100 
+// aggiungiamo un index <= a 100 /
 
 
 - Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// calcolo che va fatto nel css con la width
+// calcolo che va fatto nel css con la width /
 
 
 - Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
